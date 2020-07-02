bcc511c68095ce778e1ecb8e277843ab1c2134d2
QLMicrosoftLaptop <lqian1995@qq.com>
2020-07-02 08:53:00 +0800

    feat: add Single-Sided Amplitude Spectrum.

--- codes/ChirpDetector.m
+++ codes/ChirpDetector.m
@@ -1,58 +1,122 @@
-% Sample Rate in Hz
+% Sampling frequency in Hz
 % Audio CD
-m44100SR=44100;
-% Chirp Signal Duration in ms
-mChirpLength = 45;
-% Time Resolution in s
-mAudioCDTR = 1 /m44100SR;
+m44100SR = 44100;
+mSampleRate = m44100SR;
+% Chirp Signal Duration in s
+mChirpLength = 0.045;
+% Sampling period in s
+mAudioTimeResolution = 1 / mSampleRate;
+% Length of chirp signal
+mChirpSignalLength = ceil(mChirpLength / mAudioTimeResolution);
 % Chirp Signal Time Axis in s
-mAudioCDTA = (0:mAudioCDTR:(mChirpLength/1000))';
+mAudioSamplingAxis = 0:mChirpSignalLength - 1;
+mAudioTimeAxis = mAudioSamplingAxis * mAudioTimeResolution;
 % Instantaneous Frequency
-mFrequencyView0=15000;
-mFrequency16=16000;
-mFrequency21=21000;
-mFrequencyView1=22000;
-% Window Size
-mCSSize=size(mAudioCDTA,1);
-mWSize=2^ceil(log2(mCSSize));
-% Chirp Generation
-mAudioCDTRCS = chirp(mAudioCDTA, mFrequency16, mAudioCDTA(end), mFrequency21);
-% Chirp Signal Templete
-m44100CST=zeros(mWSize*2,1);
-m44100CST(1:length(mAudioCDTRCS),1)=mAudioCDTRCS;
-m44100CSTFFT=fft(m44100CST);
+mFrequency15k = 15000;
+mFrequency16k = 16000;
+mFrequency17k = 17000;
+mFrequency18k = 18000;
+mFrequency21k = 21000;
+mFrequency22k = 22000;
+mFrequency24k = 24000;
+
+% Chirp generation
+mChirpFreqBegin = mFrequency15k;
+mChirpFreqEnd = mFrequency24k;
+mChirpSignal = chirp(mAudioTimeAxis, mChirpFreqBegin, mAudioTimeAxis(end), mChirpFreqEnd);
+
+% Chirp signal templete
+mChirpSignalTempleteSize = 2^nextpow2(mChirpSignalLength);
+mChirpSignalTempleteAxis = 0:mChirpSignalTempleteSize - 1;
+mChirpSignalTempleteTimeAxis = mChirpSignalTempleteAxis * mAudioTimeResolution;
+mChirpSignalTemplete = zeros(mChirpSignalTempleteSize, 1);
+mChirpSignalTemplete(1:length(mChirpSignal), 1) = mChirpSignal;
+
+%%% Fiqure
+% chirpTimeDomainFigureName = sprintf("Audio Chirp Signal Templete Time Domain Window (%s Samples)", num2str(mChirpSignalTempleteSize));
+% figure('Name', chirpTimeDomainFigureName);
+% line(mChirpSignalTempleteTimeAxis * 1000, mChirpSignalTemplete);
+% ax1 = gca; % current axes
+% % ax1.XColor = 'r';
+% % ax1.YColor = 'r';
+% ax1_pos = ax1.Position; % position of first axes
+% ax2 = axes('Position',ax1_pos,...
+%     'XAxisLocation','top',...
+%     'YAxisLocation','right',...
+%     'Color','none');
+% line(mChirpSignalTempleteAxis, mChirpSignalTemplete, 'Parent', ax2)
+
+mFftSize = mChirpSignalTempleteSize;
+mChirpSignalTempleteFreqFftSamplingAxis = 1:mFftSize;
+mChirpSignalFft = fft(mChirpSignalTemplete, mFftSize);
+mChirpSignalMagnitudeSpectrum = 20 * log10(abs(mChirpSignalFft));
+%%% Fiqure
+% chirpFreqDomainFigureName = sprintf("Magnitude Spectrum of Audio Chirp Signal Templete (in dB) (FFT %s Samples)", num2str(mFftSize));
+% figure('Name', chirpFreqDomainFigureName);
+% line(mChirpSignalTempleteFreqFftSamplingAxis, mChirpSignalMagnitudeSpectrum);
+% xlabel('Sample');
+% ylabel('Magnitude Spectrum (dB)');
+
+mAudioFftResolution = mSampleRate / mFftSize;
+mChirpSignalTempleteFreqSamplingAxis = 0:(mFftSize / 2);
+mChirpSignalTempleteFreqFftAxis = mChirpSignalTempleteFreqSamplingAxis * mAudioFftResolution;
+mAmplitudeSpectrum = abs(mChirpSignalFft/mFftSize);
+mAmplitudeSpectrum = mAmplitudeSpectrum(1:(mFftSize/2+1));
+mAmplitudeSpectrum(2:end-1) = 2*mAmplitudeSpectrum(2:end-1);
+mMagnitudeSpectrum = 20 * log10(abs(mChirpSignalFft));
+mMagnitudeSpectrum = mMagnitudeSpectrum(1:(mFftSize/2+1));
+%%% Fiqure
+chirpSingleSidedFreqDomainFigureName = sprintf("Single-Sided Amplitude Spectrum of Audio Chirp Signal Templete  (%s Samples)", num2str(mFftSize));
+figure('Name', chirpFreqDomainFigureName);
+line(mChirpSignalTempleteFreqFftAxis, mMagnitudeSpectrum, 'Color', 'r');
+ax1 = gca; % current axes
+ax1.XColor = 'r';
+ax1.YColor = 'r';
+ax1.XLabel.String = 'Frequence(Hz)';
+ax1.YLabel.String = 'Magnitude Spectrum(dB)';
+ax1.XLim = [0 (mFftSize/2+1)*mAudioFftResolution];
+ax1_pos = ax1.Position; % position of first axes
+ax2 = axes('Position',ax1_pos,...
+    'XAxisLocation','top',...
+    'YAxisLocation','right',...
+    'Color','none');
+line(mChirpSignalTempleteFreqSamplingAxis, mAmplitudeSpectrum, 'Parent', ax2)
+ax2.XLabel.String = 'Sample';
+ax2.YLabel.String = 'Amplitude Spectrum';
+ax2.XLim = [0 mFftSize/2+1];
 
 % Data Set
-mFileName='ttttfilter';
-mFilePath=['D:\Graduated\博士研究相关\室内定位开发\Samples\Audio\44100\MultiPath\' mFileName '.wav'];
-[tDS,tSR]=audioread(mFilePath);
-mDSSize=size(tDS,1);
-% RAW Data Figure
-% Data Set Time Axis
-tDSTAFig=(0:mDSSize-1)/tSR;
-figure('Name','Audio Signal to be Detected');
-plot(tDSTAFig,tDS);
-xlabel('Time(s)');
+% mFileName='ttttfilter';
+% mFilePath=['D:\Graduated\博士研究相关\室内定位开发\Samples\Audio\44100\MultiPath\' mFileName '.wav'];
+% [tDS,tSR]=audioread(mFilePath);
+% mDSSize=size(tDS,1);
+%%% RAW Data Figure
+
+%%% Data Set Time Axis
+% tDSTAFig=(0:mDSSize-1)/tSR;
+% figure('Name','Audio Signal to be Detected');
+% plot(tDSTAFig,tDS);
+% xlabel('Time(s)');
 
 % 
-mWNum=floor(mDSSize/mWSize)-1;
-
-for i = 0:mWNum
-    tStartWI=i*mWSize+1;
-    tEndWI=tStartWI+mWSize*2-1;
-    tUnknownS=tDS(tStartWI:tEndWI);
-    tUnknownSTAFig=tDSTAFig(tStartWI:tEndWI);
-    
-    tUnknownSFFT=fft(tUnknownS);
-    
-    mGCCFFT=tUnknownSFFT./m44100CSTFFT;
-    
-    mGCCiFFT=ifft(mGCCFFT);
-    
-    figure('Name','Audio Signal Detecting Window');
-    plot(tUnknownSTAFig,tUnknownS,tUnknownSTAFig,mGCCiFFT);
-    xlabel('Time(s)');
-    
-    
-end
+% mWNum=floor(mDSSize/mWSize)-1;
+% 
+% for i = 0:mWNum
+%     tStartWI=i*mWSize+1;
+%     tEndWI=tStartWI+mWSize*2-1;
+%     tUnknownS=tDS(tStartWI:tEndWI);
+%     tUnknownSTAFig=tDSTAFig(tStartWI:tEndWI);
+%     
+%     tUnknownSFFT=fft(tUnknownS);
+%     
+%     mGCCFFT=tUnknownSFFT./m44100CSTFFT;
+%     
+%     mGCCiFFT=ifft(mGCCFFT);
+%     
+%     figure('Name','Audio Signal Detecting Window');
+%     plot(tUnknownSTAFig,tUnknownS,tUnknownSTAFig,mGCCiFFT);
+%     xlabel('Time(s)');
+%     
+%     
+% end
 
--- codes/GCCAnalysis.m
+++ codes/GCCAnalysis.m
@@ -1,64 +1,67 @@
 % Sample Rate in Hz
 % Audio CD
 m44100SR = 44100;
+mSampleRate = m44100SR;
 % Chirp Signal Duration in ms
 mChirpLength = 45;
 % Time Resolution in s
 mTR=1/1000;
-mAudioCDTR = 1 /m44100SR;
+mAudioCDTR = 1 / m44100SR;
 % Chirp Signal Time Axis in s
-mAudioCDTA = (0 : 1 / m44100SR : (mChirpLength / 1000))';
+mAudioTimeAxis = (0 : 1 / mSampleRate : (mChirpLength / 1000))';
 % Instantaneous Frequency
-mFrequencyView0 = 15000;
-mFrequency16 = 16000;
-mFrequency17 = 17000;
-mFrequency18 = 18000;
-mFrequency21 = 21000;
-mFrequencyView1 = 22000;
+mFrequency15k = 15000;
+mFrequency16k = 16000;
+mFrequency17k = 17000;
+mFrequency18k = 18000;
+mFrequency21k = 21000;
+mFrequency22k = 22000;
+mFrequency24k = 24000;
 % Window Size
-mCSSize=size(mAudioCDTA,1);
-mWSize=2^ceil(log2(mCSSize));
+mChirpSignalSize=size(mAudioTimeAxis,1);
+mWindowSize=2^ceil(log2(mChirpSignalSize));
 % Frequence Index
-m17kFI=floor((mFrequency17-mFrequency16)*mCSSize/(mFrequency21-mFrequency16));
-m18kFI=floor((mFrequency18-mFrequency16)*mCSSize/(mFrequency21-mFrequency16));
+m17kFI=floor((mFrequency17k-mFrequency16k)*mChirpSignalSize/(mFrequency21k-mFrequency16k));
+m18kFI=floor((mFrequency18k-mFrequency16k)*mChirpSignalSize/(mFrequency21k-mFrequency16k));
 
 % Chirp Generation
-mAudioCDTRCS = chirp(mAudioCDTA, mFrequency16, mAudioCDTA(end), mFrequency21);
-mAudioCDTRCS17k21k = mAudioCDTRCS(m17kFI:end);
-mAudioCDTRCS17k21kTA=mAudioCDTA(m17kFI:end);
+
+mAudioTRCS = chirp(mAudioTimeAxis, mFrequency16k, mAudioCDTA(end), mFrequency21k);
+mAudioCDTRCS17k21k = mAudioTRCS(m17kFI:end);
+mAudioCDTRCS17k21kTA = mAudioCDTA(m17kFI:end);
 
 % Chirp Signal Templete
-m44100CST=randn(mWSize,1)/100;
-m44100CST(1:length(mAudioCDTRCS),1)=mAudioCDTRCS;
+m44100CST=randn(mWindowSize,1)/100;
+m44100CST(1:length(mAudioTRCS),1)=mAudioTRCS;
 
 % Chirp Signal Detector Templete
-m44100CS17kDT=randn(mWSize,1)/100;
+m44100CS17kDT=randn(mWindowSize,1)/100;
 m44100CS17kDT(1:length(mAudioCDTRCS17k21k),1)=mAudioCDTRCS17k21k;
 
 % Frequency Domain
 m44100CSTFD=fft(m44100CST);
 m44100CS17kDTFD=fft(m44100CS17kDT);
 
-figure('Name','Standard Chirp Signal 16k to 21k Frequency Domain');
-pspectrum(mAudioCDTRCS,mAudioCDTA,'FrequencyLimits',[mFrequencyView0 mFrequencyView1]);
-figure('Name','Standard Chirp Signal 17k to 21k Frequency Domain');
-pspectrum(mAudioCDTRCS17k21k,mAudioCDTRCS17k21kTA,'FrequencyLimits',[mFrequencyView0 mFrequencyView1])
+% figure('Name','Standard Chirp Signal 16k to 21k Frequency Domain');
+% pspectrum(mAudioCDTRCS,mAudioCDTA,'FrequencyLimits',[mFrequencyView0 mFrequencyView1]);
+% figure('Name','Standard Chirp Signal 17k to 21k Frequency Domain');
+% pspectrum(mAudioCDTRCS17k21k,mAudioCDTRCS17k21kTA,'FrequencyLimits',[mFrequencyView0 mFrequencyView1])
 
-figure('Name','Standard Chirp Signal 16k to 21k Spectrogram');
-pspectrum(mAudioCDTRCS,mAudioCDTA,'spectrogram','FrequencyLimits',[mFrequencyView0 mFrequencyView1],'TimeResolution',mTR);
-figure('Name','Standard Chirp Signal 17k to 21k Spectrogram');
-pspectrum(mAudioCDTRCS17k21k,mAudioCDTRCS17k21kTA,'spectrogram','FrequencyLimits',[mFrequencyView0 mFrequencyView1],'TimeResolution',mTR)
+% figure('Name','Standard Chirp Signal 16k to 21k Spectrogram');
+% pspectrum(mAudioCDTRCS,mAudioCDTA,'spectrogram','FrequencyLimits',[mFrequencyView0 mFrequencyView1],'TimeResolution',mTR);
+% figure('Name','Standard Chirp Signal 17k to 21k Spectrogram');
+% pspectrum(mAudioCDTRCS17k21k,mAudioCDTRCS17k21kTA,'spectrogram','FrequencyLimits',[mFrequencyView0 mFrequencyView1],'TimeResolution',mTR)
 
 
 % 
-mGCCFD=m44100CSTFD./m44100CS17kDTFD;
+% mGCCFD=m44100CSTFD./m44100CS17kDTFD;
 
 % Time Domain
-mGCCTD=ifft(mGCCFD);
+% mGCCTD=ifft(mGCCFD);
 
 
 
-mGCCTDModulus=abs(mGCCTD);
+% mGCCTDModulus=abs(mGCCTD);
 
 % Time Delay
-[mCORRCOEF,mTimeDelay]=max(mGCCTDModulus);
+% [mCORRCOEF,mTimeDelay]=max(mGCCTDModulus);
